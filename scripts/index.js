e7345c8064395df05f6108001d2e1936fc784618
Riuku Usami <ne211069@senshu-u.jp>
2023-07-19 00:01:11 +0900

    aaa

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,28 @@
 
+const Jimp = require('jimp');
+
 module.exports = (robot) => {
-  let questionSentId = {};
-
-  robot.respond(/PING$/i, (res) => {
-    res.send({
-      question: '好きな果物は？',
-      options: ['りんご', 'いちご', '梨', 'オレンジ'],
-      onsend: (sent) => {
-        questionSentId[res.message.rooms[res.message.room].id] = sent.message.id;
-      }
-    });
-  });
-
-  robot.respond('select', (res) => {
-    if (res.json.response === null) {
-      res.send(`Your question is ${res.json.question}.`);
-    } else {
-      res.send({
-        text: `あなたは ${res.json.options[res.json.response]} が好きなんですね.`,
-        onsend: (sent) => {
-          res.send({
-            close_select: questionSentId[res.message.rooms[res.message.room].id]
+  const onfile = (res, file) => {
+    res.download(file, (path) => {
+      let ext = file.name.slice(-4);
+      Jimp.read(path).then((image) => {
+        image.grayscale((err, image) => {
+          let newFileName = Math.random().toString(32).substring(2) + ext;
+          image.write('images/' + newFileName, (err, image) => {
+            res.send({
+              path: 'images/' + newFileName
+            });
           });
-        }
+        });
       });
-    }
-  });
+    });
+  };
 
+  robot.respond('file', (res) => {
+    onfile(res, res.json);
+  });
 };
+
 
 // const Jimp = require('jimp');
 // const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
